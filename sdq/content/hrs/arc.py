dac08612a27b8726c458591bae91f8b0e6dd2a89
Eb-Zeero <macebelene@gmail.com>
2018-11-02 15:48:56 +0200

    previous arc testing

--- sdq/content/hrs/arc.py
+++ sdq/content/hrs/arc.py
@@ -1,12 +1,13 @@
 from bokeh.plotting import figure
-from dq_poc.util import plot_grid
+from flask import g
+from sdq.util import bokeh_plot_grid
 from sdq.queries.hrs import arc_query
 
 
-def plot(start_date, end_date, obs_mode, arm):
-    source = arc_query(start_date, end_date, obs_mode, arm)
+def plot(obs_mode, arm):
+    source = arc_query(str(g.dates['start_date']), str(g.dates['end_date']), obs_mode, arm)
 
-    p = figure(plot_height=1500, plot_width=2000, title=arm.upper() + ' ' + obs_mode, x_axis_label='Date',
+    p = figure(plot_height=150, plot_width=200, title=arm.upper() + ' ' + obs_mode, x_axis_label='Date',
                y_axis_label='AVG(DeltaX)', x_axis_type='datetime',)
     p.scatter(source=source, x='UTStart', y='avg', color='plasma_colors', fill_alpha=0.2, size=10)
 
@@ -15,14 +16,14 @@ def plot(start_date, end_date, obs_mode, arm):
 
 title = 'HRS Arc Waves'
 
-content = plot_grid(2,
-                    plot('2018-07-01', '2018-08-31', 'HIGH RESOLUTION', 'blue'),
-                    plot('2018-07-01', '2018-08-31', 'HIGH RESOLUTION', 'red'),
-                    plot('2018-07-01', '2018-08-31', 'MEDIUM RESOLUTION', 'blue'),
-                    plot('2018-07-01', '2018-08-31', 'MEDIUM RESOLUTION', 'red'),
-                    plot('2018-07-01', '2018-08-31', 'LOW RESOLUTION', 'blue'),
-                    plot('2018-07-01', '2018-08-31', 'LOW RESOLUTION', 'red'),
+content = bokeh_plot_grid(2,
+                          plot('HIGH RESOLUTION', 'blue'),
+                          plot('HIGH RESOLUTION', 'red'),
+                          plot('MEDIUM RESOLUTION', 'blue'),
+                          plot('MEDIUM RESOLUTION', 'red'),
+                          plot('LOW RESOLUTION', 'blue'),
+                          plot('LOW RESOLUTION', 'red'),
 
-                    )
+                          )
 
 description = 'Arc waves fro HRS'
