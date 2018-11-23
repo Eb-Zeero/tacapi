1ae8d9b6d67a872f1060ce66fefa839df38b13cb
Eb-Zeero <macebelene@gmail.com>
2018-11-23 12:57:23 +0200

    flask make response to update cookies

--- sdq/diagnostic_table.py
+++ sdq/diagnostic_table.py
@@ -1,3 +1,59 @@
+from sdq.util import grouper
+from sdq.data import data_t
+
+
+def table_head(dates):
+    head = ''
+    for day in dates:
+        if None not in day:
+            head += '<th colspan="{length}">{day1} to {day2}</th>'\
+                .format(day1=day[0],
+                        day2=day[len(day)-1],
+                        length=len(day))
+        else:
+            dayx = [x for x in day if x is not None]
+            head += '<th colspan="{length}">{day1} to {day2}</th>'\
+                .format(day1=day[0],
+                        day2=dayx[len(dayx)-1],
+                        length=len(day))
+
+    return head
+
+
+def table_body(data):
+    body = ''
+    for key, row in data.items():
+        name=key
+        body += '<tr><td>{name}</td>'.format(name=name)
+        for s in row:
+            try:
+                if s['color'] is None or s['color'] not in ['red', 'green', 'orange', 'yellow', 'grey']:
+                    body += '<td></td>'
+                else:
+                     body += '<td><b class="tip" style="background-color: {color}; color: {color};">' \
+                             '..' \
+                             '<span>name: {name} <br/>date: {date}</span>' \
+                             '</b></td>'\
+                        .format(color=s['color'], date=s['date'], name=name)
+            except TypeError:
+                pass
+        body += '</tr>'
+    return body
+
+
+def color_code(status):
+    if status == 'ok':
+        color = 'green'
+    elif status == 'fail':
+        color = 'red'
+    elif status == 'warning':
+        color = 'yellow'
+    elif status == 'noexec':
+        color = 'orange'
+    else:
+        color = 'grey'
+    return color
+
 
 def table_row(row_data):
     row = '<tr><td>{name}</td>'.format(name=row_data['name'])
@@ -49,3 +105,104 @@ def diagnostic_table(data):
   </table>
 </div>
     '''.format(head=table_head, body=table_body)
+
+
+def diagnostic_table_week(data):
+    if not isinstance(data, list):
+        return '<p style="color: red;">Fail to display this table </p>'
+    # data = [
+    #     {
+    #         'name': 'name-1',
+    #         'data': [
+    #             {
+    #                 'date': 'day1',
+    #                 'status': 'ok'
+    #             },
+    #             {
+    #                 'date': 'day2',
+    #                 'status': 'noexec'
+    #             },
+    #             {
+    #                 'date': 'day3',
+    #                 'status': 'ok'
+    #             },
+    #             {
+    #                 'date': 'day4',
+    #                 'status': 'ok'
+    #             }
+    #         ]
+    #     },
+    #     {'name': 'name-2', 'data': [{'date': 'day1', 'status': 'fail'}, {'date': 'day2', 'status': 'ok'},
+    #                                 {'date': 'day3', 'status': 'noexec'}, {'date': 'day4', 'status': 'ok'},
+    #                                 {'date': 'day5', 'status': 'noexec'}, {'date': 'day8', 'status': 'ok'},
+    #                                 {'date': 'day6', 'status': 'noexec'}, {'date': 'day9', 'status': 'ok'},
+    #                                 {'date': 'day7', 'status': 'noexec'}, {'date': 'day90', 'status': 'ok'}
+    #                                 ]},
+    #     {'name': 'name-3', 'data': [{'date': 'day1', 'status': 'noexec'}, {'date': 'day2', 'status': 'fail'},
+    #                                 {'date': 'day3', 'status': 'noexec'}, {'date': 'day4', 'status': 'noexec'},
+    #                                 {'date': 'day5', 'status': 'ok'}, {'date': 'day9', 'status': 'noexec'}
+    #                                 ]},
+    #     {'name': 'name-4', 'data': [{'date': 'day1', 'status': 'ok'}, {'date': 'day2', 'status': 'warning'},
+    #                                 {'date': 'day3', 'status': 'fail'}, {'date': 'day4', 'status': 'ok'},
+    #                                 {'date': 'day5', 'status': 'ok'}, {'date': 'day7', 'status': 'noexec'}
+    #                                 ]},
+    #     {'name': 'name-5', 'data': [{'date': 'day1', 'status': 'warning'}, {'date': 'day2', 'status': 'ok'},
+    #                                 {'date': 'day3', 'status': 'ok'}, {'date': 'day4', 'status': 'ok'}
+    #                                 ]},
+    #     {'name': 'name-6',
+    #      'data': [{'date': 'day1', 'status': 'ok'}, {'date': 'day2', 'status': 'ok'}, {'date': 'day3', 'status': 'ok'},
+    #               {'date': 'day4', 'status': 'warning'}]}
+    # ]
+
+    data = data_t()
+    map_array = {}
+    dt = []
+    nm = []
+    for d in data:
+        if d['name'] not in nm:
+            nm.append(d['name'])
+        for day in d['data']:
+            if day['date'] not in dt:
+                dt.append(day['date'])
+
+    nm.sort()
+    dt.sort()
+    dt = [list(elem) for elem in list(grouper(7, dt))]
+    final_data = {}
+
+
+    for name in nm:
+        date_copy = []
+        for week in dt:
+            for d in week:
+                date_copy.append({'date': d, 'color': 'white'})
+        final_data[name] = date_copy
+
+    for value in data:
+        for d in value['data']:
+            for f in final_data[value['name']]:
+                if d['date'] == f['date']:
+                    f['color'] = color_code(d['status'])
+
+
+
+    thead = table_head(dt)
+    tbody = table_body(final_data)
+    plot = '''
+<div class="container" style="overflow-x:auto; width: 90%; height: 90%;" >
+  <table  class='table is-striped  is-bordered' style="overflow: scroll;">
+    <thead>
+      <tr>
+        <td>Name</td>{head}
+      </tr>
+    </thead>
+    <tbody>{body}</tbody>
+    <tfoot>
+      <tr>
+        <td>Name</td>{head}
+      </tr>
+    </tfoot>
+  </table>
+</div>
+    '''.format(head=thead, body=tbody)
+    return plot
